3df0c69236f339dfc4dc94fca5a2af4e4175296a
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2020-10-11 19:40:38 +0200

    Added forecast

--- src/app.js
+++ src/app.js
@@ -22,6 +22,20 @@ let currentTime = document.querySelector("#current-time");
 
 currentTime.innerHTML = `Last updated: ${day}, ${hours}:${minutes}`;
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
 
 // Real-time Weather
 
@@ -49,6 +63,36 @@ function displayTemp(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+// Forecast
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>
+  `;
+  }
+}
+
 //Change City
 
 function search(city) {
@@ -56,6 +100,8 @@ let apiKey = "666e992c8bf5317be35ba26eb820d6ec";
 let units = "metric";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
 axios.get(apiUrl).then(displayTemp);
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${apiKey}`;
+axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -135,8 +181,6 @@ function getCurrentLocation(event) {
 
 let locationButton = document.querySelector("#current-location-btn");
 locationButton.addEventListener("click", getCurrentLocation);
-
-// Forecast
 
 
 search("Berlin");
